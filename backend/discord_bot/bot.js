8165a43a3e3af0086901b6fa0d899d0f0f4a55d3
masterMav <mavdoesdev@gmail.com>
2024-04-22 17:21:14 +0530

    bot completed

--- backend/discord_bot/bot.js
+++ backend/discord_bot/bot.js
@@ -1,5 +1,8 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const colors = require("colors");
+const Blog = require("../models/blog");
+const Problem = require("../models/problem");
+const Fuse = require("fuse.js");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -12,8 +15,13 @@ module.exports = function initBot() {
     ],
   });
 
-  client.on("ready", () => {
+  let allBlogs;
+  let allProblems;
+
+  client.on("ready", async () => {
     console.log("Bot is running.".cyan.underline);
+    allBlogs = await Blog.find();
+    allProblems = await Problem.find();
   });
 
   client.on("messageCreate", (message) => {
@@ -22,8 +30,49 @@ module.exports = function initBot() {
     if (message.content.startsWith(";search_blogs ")) {
       const queryText = message.content.split(";search_blogs ")[1];
 
+      const fuse = new Fuse(allBlogs, {
+        keys: ["title"],
+      });
+
+      const searchResult = fuse.search(queryText, { limit: 5 });
+
+      const blogs = [];
+
+      searchResult.forEach((item, index) => {
+        blogs.push(`${index + 1}. ${item.item.title.toString()}`);
+        blogs.push(`\t Author: ${item.item.authorName.toString()}`);
+      });
+
+      const str = blogs.join("\n");
+
       return message.reply({
-        content: "got query for" + queryText,
+        content: str,
+      });
+    }
+
+    if (message.content.startsWith(";search_problem ")) {
+      const queryText = message.content.split(";search_problem ")[1];
+
+      const fuse = new Fuse(allProblems, {
+        keys: ["problemCode", "problemName"],
+      });
+
+      const searchResult = fuse.search(queryText, { limit: 5 });
+
+      const problems = [];
+
+      searchResult.forEach((item, index) => {
+        problems.push(`${index + 1}. ${item.item.problemName.toString()} -`);
+
+        problems.push(
+          `\t Rating: ${item.item.problemRating.toString()} \t Code: ${item.item.problemCode.toString()}`
+        );
+      });
+
+      const str = problems.join("\n");
+
+      return message.reply({
+        content: str,
       });
     }
   });
